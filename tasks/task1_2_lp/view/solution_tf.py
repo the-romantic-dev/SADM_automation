30fbb028640d8e35e07ec1ac14bf1cfe3ea95ce6
the-romantic-dev <ahxplay@gmail.com>
2024-08-23 12:51:44 +0300

    Сделал в ответе отображение переменных в порядке возрастания индекса

--- tasks/task1_2_lp/view/solution_tf.py
+++ tasks/task1_2_lp/view/solution_tf.py
@@ -6,6 +6,7 @@ from tasks.task1_2_lp.model.basis_solution.basis_solution import BasisSolution
 from tasks.task1_2_lp.viewmodel.basis_solution_viewmodel import BasisSolutionViewModel
 
 
+
 class SolutionTF(TemplateFiller):
     def __init__(self, template: DocumentTemplate):
         super().__init__(template)
@@ -13,15 +14,30 @@ class SolutionTF(TemplateFiller):
     @staticmethod
     def result_formula(opt_solution: BasisSolution):
         best_solution_vm = BasisSolutionViewModel(opt_solution)
+
+        basis = opt_solution.basis
+        free = opt_solution.free
+        total_vars = len(basis) + len(free)
+
         basis_variables = best_solution_vm.basis_variables_latex
         free_variables = best_solution_vm.free_variables_latex
-        f_variable = 'f'
-        variables = basis_variables + free_variables + [f_variable]
+        variables = []
 
         basis_values = [rational_latex(value) for value in opt_solution.basis_values]
         free_values = ["0" for _ in range(len(opt_solution.free))]
-        f_value = rational_latex(opt_solution.objective_value)
-        values = basis_values + free_values + [f_value]
+        values = []
+        for i in range(total_vars):
+            if i in basis:
+                bi = basis.index(i)
+                variables.append(basis_variables[bi])
+                values.append(basis_values[bi])
+            else:
+                fi = free.index(i)
+                variables.append(free_variables[fi])
+                values.append(free_values[fi])
+
+        variables += ['f']
+        values += [rational_latex(opt_solution.objective_value)]
 
         formula_latex = ",".join(f"{variables[i]} = {values[i]}" for i in range(len(variables)))
         return Formula(formula_latex)
